080487c70c8821f217f7f0e8b06992aa43d9ad29
My Name <sdasd@yandex.ru>
2023-08-03 14:26:26 +0500

    task1: Переделала класс генерации документов

--- src/ru/julia/DocumentGeneration.java
+++ src/ru/julia/DocumentGeneration.java
@@ -4,19 +4,25 @@ import java.util.*;
 
 public class DocumentGeneration {
     DocumentFactory documentFactory = new DocumentFactory();
-    private final List<Document> FACTORY = Arrays.asList(documentFactory.createOutgoingDocument(), documentFactory.createIncomingDocument(), documentFactory.createTaskDocument());
     public List<Document> documents = new ArrayList<>();
     private final List<String> AUTHORS = new ArrayList<>();
     public Map<String, List<Document>> authorAndDocument = new HashMap<>();
 
     public void generateDocuments() {
         for (int i = 0; i < (int) (Math.random() * 10); i++) {
-            documents.add(FACTORY.get((int) (Math.random() * 3)));
+            if (i < 5) {
+                documents.add(documentFactory.createOutgoingDocument());
+            } else if (i > 5) {
+                documents.add(documentFactory.createTaskDocument());
+            } else {
+                documents.add(documentFactory.createIncomingDocument());
+            }
         }
     }
+
     public void convertListToMap(List<Document> documents) {
         for (Document document : documents) {
-            if (!AUTHORS.contains(document.author)){
+            if (!AUTHORS.contains(document.author)) {
                 AUTHORS.add(document.author);
             }
         }
